429348406f96ba1cac09d78eee3416c1ca6c7db6
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-25 14:27:39 +0100

    Add temperature task

--- question16.js
+++ question16.js
@@ -1,1 +1,8 @@
 let temperatures = [30, 28, 35, 22, 31];
+let highest = temperatures[0];
+for (let i = 1; i < temperatures.length; i++) {
+  if (temperatures[i] > highest) {
+    highest = temperatures[i];
+  }
+}
+console.log(highest);
