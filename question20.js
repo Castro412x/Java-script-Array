82015440660b48601cecc3d2499713a5ae2d867c
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-26 09:26:58 +0100

    Add team task

---
+++ question20.js
@@ -0,0 +1,5 @@
+let team = ["Ronaldo","Messi","Mbappe","Salah","Kante"];
+for(i = 0; i < team.length; i++){
+    console.log(`Player ${i + 1}: ${team[i]}`)
+}
+comsole.log(`Total players = ${team.length}`)
